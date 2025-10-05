2fe42b74e780e188234fd82273048459875f6c80
infraredracoon1 <72762386+infraredracoon1@users.noreply.github.com>
2025-10-04 23:58:42 -0500

    Create constants.py
    Signed-off-by: infraredracoon1 <72762386+infraredracoon1@users.noreply.github.com>

---
+++ uam/engines/constants.py
@@ -0,0 +1,8 @@
+nu = 1e-3                  # viscosity
+C1 = 0.101                 # quadratic term coefficient
+C2 = 3.75e-16              # cubic damping coefficient
+kappa_NS = 0.105
+kappa_YM = 0.12
+lambda_NS = 0.0096
+lambda_YM = 0.0090
+gamma = 0.8                # spectral gap
